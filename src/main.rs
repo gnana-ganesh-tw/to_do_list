a3652426f043e5b91430b94f1f51e617bb8a7bf4
Gnana Ganesh <gnana.ganesh@thoughtworks.com>
2023-03-07 17:08:49 +0530

    Add. First todo in plain string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let first_todo = "complete DS assignment";
+    let first_todo_status = "pending";
+    let first_todo_deadline = "01/04/2023";
+
+    println!("Todo: {}, Status: {}, Date: {}", first_todo, first_todo_status, first_todo_deadline);
 }
